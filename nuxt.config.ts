e534d5a05c1a5a28bb2befb01c27322edf9e0cbc
rofif <mrofiiftaqiyyuddinn@student.unmul.ac.id>
2026-02-06 10:35:40 +0800

    av'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui',
-    'nuxt-auth-utils',
-    '@nuxt/icon'
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-auth-utils", "@nuxt/icon"],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
-    '/': { prerender: true }
+    "/": { prerender: false },
   },
 
-  compatibilityDate: '2025-01-15',
+  nitro: {
+    precompile: [],
+  },
+
+  compatibilityDate: "2025-01-15",
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-})
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+});
